b5520860d0cb99766b42e2c4abfd68d2492567b7
Eugene Feinberg <eugene@recogni.com>
2020-02-27 08:05:12 -0800

    2 bug fixes for _rdy handling
    - Multi cycle registers had an incorrect rdy -> data dependency
    - Single cycle didn't check for _rdy

--- src/ordt/output/systemverilog/decode/SystemVerilogDecodeModule.java
+++ src/ordt/output/systemverilog/decode/SystemVerilogDecodeModule.java
@@ -2351,14 +2351,16 @@ public class SystemVerilogDecodeModule extends SystemVerilogModule {
 
 
 		if (useDataCounter) {
-			this.addCombinAssign(groupName,     "      if ( !" +  ringResRdyName + ") begin ");
+			this.addCombinAssign(groupName,     "      if ( !" +  ringResRdyName + " ) begin ");
 			this.addCombinAssign(groupName,     "        " + ringDataCntNextName + " = " + ringDataCntName + ";" );
-	
 			this.addCombinAssign(groupName,     "      end else begin" );
 
 			//  bump the data count
 			this.addCombinAssign(groupName,     "      " + ringDataCntNextName + " = " + ringDataCntName + " + " + maxDataXferCountBits + "'b1;");
 			// send the data slice while 
+			this.addCombinAssign(groupName,     "      end"); 
+
+
 			for (int idx=0; idx<maxDataXferCount; idx++) {
 				prefix = (idx == 0)? "" : "else ";
 				this.addCombinAssign(groupName, "      "+ prefix + "if (" + ringDataCntName + " == " + maxDataXferCountBits + "'d" + idx + ")");  
@@ -2368,11 +2370,12 @@ public class SystemVerilogDecodeModule extends SystemVerilogModule {
 			// if final count we're done
 			String finalRetCntStr = getSerialMaxDataCountStr(useTransactionSize, transactionsInWord, pioInterfaceRetTransactionSizeName);
 			this.addCombinAssign(groupName, "      if (" + ringDataCntName + " == " + finalRetCntStr + ") " + ringStateNextName + " = " + IDLE + ";");
-			this.addCombinAssign(groupName,     "      end"); 
 		}
 		else {
 			this.addCombinAssign(groupName, "      " + resDataDlyName[0] + " = " + ringRdCaptureName + SystemVerilogSignal.genRefArrayString(0, ringWidth) + ";");
-			this.addCombinAssign(groupName, "      " + ringStateNextName + " = " + IDLE + ";");	
+			this.addCombinAssign(groupName, "      if ( " +  ringResRdyName + " ) begin ");
+			this.addCombinAssign(groupName, "          " + ringStateNextName + " = " + IDLE + ";");	
+			this.addCombinAssign(groupName, "      end"); 
 		}
 
 		this.addCombinAssign(groupName,     "    end"); 
